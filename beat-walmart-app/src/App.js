1b1a7e465ed3a05504d054d7c07722b480697426
nikitamenon97 <menon.nikita97@gmail.com>
2022-12-10 14:24:10 -0700

    tooltip fetching from json file

---
+++ beat-walmart-app/src/App.js
@@ -0,0 +1,22 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import React, { useState } from "react";
+// import ReactTooltip from "react-tooltip";
+
+// import "./App.css";
+
+// import MapChart from "./MapChart";
+
+// function App() {
+//   const [content, setContent] = useState("CO");
+//   return (
+//     <div>
+//       {/* <MapChart /> */}
+//       <MapChart setTooltipContent={setContent} />
+//       <ReactTooltip offset={{ top: 150, left: 150 }}>Hellooooo{content}</ReactTooltip>
+//     </div>
+//   );
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
---
+++ beat-walmart-app/src/navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<NavLink to='/' activeStyle>
+			Home
+		</NavLink>
+		{/* Second Nav */}
+		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+		</NavMenu>
+		<NavBtn>
+		<NavBtnLink to='/'>Sign In</NavBtnLink>
+		</NavBtn>
+	</Nav>
+	</>
+);
+};
+
+export default Navbar;
