0bba5a285e115403bc0a9757d83a5c246ddca986
nikitamenon97 <menon.nikita97@gmail.com>
2022-12-10 14:24:10 -0700

    tooltip fetching from json file

---
+++ beat-walmart-app/public/info.js
@@ -0,0 +1,5 @@
+export const test_data = [
+    {
+      "test_key": "test_value"  
+    }
+]
---
+++ beat-walmart-app/src/MapChart.js
@@ -0,0 +1,123 @@
+import React from "react";
+import {useState} from 'react';
+import { geoCentroid } from "d3-geo";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  Annotation
+} from "react-simple-maps";
+
+import allStates from "./data/allstates.json";
+import data from "./data.json";
+
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
+
+const offsets = {
+  VT: [50, -8],
+  NH: [34, 2],
+  MA: [30, -1],
+  RI: [28, 2],
+  CT: [35, 10],
+  NJ: [34, 1],
+  DE: [33, 0],
+  MD: [47, 10],
+  DC: [49, 21]
+};
+
+const markers = [
+    {
+      markerOffset: -30,
+      name: "Buenos Aires",
+      coordinates: [-58.3816, -34.6037]
+    },
+    { markerOffset: 15, name: "La Paz", coordinates: [-68.1193, -16.4897] }
+  ];
+
+const MapChart = ({ setTooltipContent }) => {
+    
+  return (
+    <div data-tip="">
+    <ComposableMap projection="geoAlbersUsa">
+      <Geographies geography={geoUrl}>
+        {({ geographies }) => (
+          <>
+            {geographies.map(geo => (
+              <Geography
+                key={geo.rsmKey}
+                stroke="#FFF"
+                geography={geo}
+                fill="#DDD"
+                onMouseEnter={() => {
+                  const cur = data.find(s => s.state === geo.properties.name);
+                  // console.log(geo, cur)
+                  setTooltipContent(`Item running low : ${cur.low_product} Quantity: ${cur.val}`);
+                  }}
+                onMouseLeave={() => {
+                setTooltipContent("");
+                }}
+                style={{
+                default: {
+                    fill: "#D6D6DA",
+                    outline: "none",
+                },
+                hover: {
+                    fill: "#E42",
+                    outline: "none"
+                },
+                pressed: {
+                    fill: "#E42",
+                    outline: "none"
+                }
+                }}
+              />
+            ))}
+            {geographies.map(geo => {
+              const centroid = geoCentroid(geo);
+              const cur = allStates.find(s => s.val === geo.id);
+              return (
+                <g key={geo.rsmKey + "-name"}>
+                  {cur &&
+                    centroid[0] > -160 &&
+                    centroid[0] < -67 &&
+                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                      <Marker coordinates={centroid}>
+                        <g
+                            fill="none"
+                            stroke="#FF5533"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            transform="translate(-12, -24)"
+                        >
+                            <circle cx="12" cy="10" r="3" />
+                            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                        </g>
+                        <text y="2" fontSize={14} textAnchor="middle">
+                          {cur.id}
+                        </text>
+                      </Marker>
+                    ) : (
+                      <Annotation
+                        subject={centroid}
+                        dx={offsets[cur.id][0]}
+                        dy={offsets[cur.id][1]}
+                      >
+                        <text x={4} fontSize={14} alignmentBaseline="middle">
+                          {cur.id}
+                        </text>
+                      </Annotation>
+                    ))}
+                </g>
+              );
+            })}
+          </>
+        )}
+      </Geographies>
+    </ComposableMap>
+    </div>
+  );
+};
+
+export default MapChart;
