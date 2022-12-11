24484bfdf2cd525551f9dade6b8d060818b72083
Nishant Bhattacharya <nishantb21@gmail.com>
2022-12-11 00:12:42 -0700

    Basic server ready

--- beat-walmart-app/server/index.js
+++ beat-walmart-app/server/index.js
@@ -4,6 +4,24 @@ const http = require("http");
 const {Server} = require("socket.io")
 const cors = require("cors")
 const server = http.createServer(app)
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'restserver',
+  brokers: ['sw-kafka.kafka-ns.svc.cluster.local:9092'],
+})
+
+const consumer = kafka.consumer({ groupId: 'test-group' })
+consumer.connect()
+consumer.subscribe({ topic: 'notifications', fromBeginning: false })
+
+consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+        value: message.value.toString(),
+        })
+    },
+})
 
 app.use(cors());
 
